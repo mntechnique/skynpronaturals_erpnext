6b4b649c4a3dc80e3ab46244a83360bfa13750d2
Gaurav Naik <gvrvnk@gmail.com>
2016-10-25 10:27:31 +0530

    Sales Invoice: Checking Warehouse state instead of name for setting series.

--- skynpronaturals_erpnext/api.py
+++ skynpronaturals_erpnext/api.py
@@ -6,24 +6,45 @@ def validate_sales_invoice(self, method):
     spn_warehouse = frappe.db.get_value("Sales Invoice","spn_warehouse")
     cust_ter = frappe.db.get_value("Sales Invoice","territory")
     cust_group = frappe.db.get_value("Sales Invoice","customer_group")
-    self.naming_series = get_naming_series(spn_warehouse,cust_ter,cust_group)
+    
+    if not self.naming_series:
+        self.naming_series = get_naming_series(spn_warehouse,cust_ter,cust_group)
 
 @frappe.whitelist()
 def get_naming_series(spn_warehouse, cust_ter, cust_group):
-    if spn_warehouse == "Bellezimo Professionale Products Pvt. Ltd. C/o. Kotecha Clearing & Forwarding Pvt. Ltd.  - SPN":
+
+    warehouse_state = frappe.db.get_value("Warehouse", spn_warehouse, "state")
+
+    if warehouse_state == "Assam":
         if cust_group=="Assam Registered Distributor" and cust_ter == "Assam":
             return "GV-.#####"
         elif cust_group=="Assam Unregistered Distributor":
             return "GU-.#####"
         else:
             return "GC-.#####"
-    elif spn_warehouse == "Maharastra":
-        if cust_group=="Maharashtra Registered Distributor" and cust_ter == "Mharashtra":
+    elif warehouse_state == "Maharashtra":
+        if cust_group=="Maharashtra Registered Distributor" and cust_ter == "Maharashtra":
             return "BV-.#####"
         elif cust_group=="Maharashtra Unregistered Distributor":
             return "BU-.#####"
         else:
             return "BC-.#####"
+    
+
+    # if spn_warehouse == "Bellezimo Professionale Products Pvt. Ltd. - SPN":
+    #     if cust_group=="Assam Registered Distributor" and cust_ter == "Assam":
+    #         return "GV-.#####"
+    #     elif cust_group=="Assam Unregistered Distributor":
+    #         return "GU-.#####"
+    #     else:
+    #         return "GC-.#####"
+    # elif spn_warehouse == "Bellezimo Professionale Products Pvt. Ltd. C/o. Kotecha Clearing & Forwarding Pvt. Ltd.  - SPN":
+    #     if cust_group=="Maharashtra Registered Distributor" and cust_ter == "Maharashtra":
+    #         return "BV-.#####"
+    #     elif cust_group=="Maharashtra Unregistered Distributor":
+    #         return "BU-.#####"
+    #     else:
+    #         return "BC-.#####"
 
 
 
