3c6c3544f359cf62f99335ad5f3e8197849298d9
Gaurav Naik <gvrvnk@gmail.com>
2016-12-22 16:22:50 +0530

    Included linked stock entry field for Stock Entry

--- skynpronaturals_erpnext/hooks.py
+++ skynpronaturals_erpnext/hooks.py
@@ -138,7 +138,7 @@ fixtures = [{"dt": "Custom Field", "filters":[["name", "in", ['Sales Invoice-spn
                                                             'Stock Entry-st_road_permit_no', 'Stock Entry Detail-standard_rate',
                                                             'Stock Entry-transport_and_payload_information', 
                                                             'Stock Entry-cb_transport_payload_info_1', 
-                                                            'Stock Entry-spn_linked_transit_entity']]]},
+                                                            'Stock Entry-spn_linked_transit_entry']]]},
              {"dt": "Custom Script", "filters":[["name", "in", ['Sales Invoice-client','Purchase Receipt-Client','Stock Entry-Client']]]},
              {"dt": "Property Setter", "filters": [["name", "in",["update_stock", "Sales Invoice Item-rate-read_only"]]]},
              {"dt": "Print Format", "filters": [["name", "in", ["SPN Sales Invoice","Memo Invoice","Goods Receipt Note","Stock Transfer Note"]]]}]
