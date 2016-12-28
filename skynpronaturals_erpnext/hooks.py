e91897a59192216ce8c9c64bf86efc53b5333e43
Gaurav Naik <gvrvnk@gmail.com>
2016-12-28 17:05:34 +0530

    Added rejected qty to Stock Entry. Changed lost qty logic

--- skynpronaturals_erpnext/hooks.py
+++ skynpronaturals_erpnext/hooks.py
@@ -142,10 +142,8 @@ fixtures = [{"dt": "Custom Field", "filters":[["name", "in", ['Sales Invoice-spn
                                                             'Stock Entry-transport_and_payload_information',
                                                             'Stock Entry-cb_transport_payload_info_1',
                                                             'Stock Entry-spn_linked_transit_entry',
-                                                            'Stock Entry Detail-spn_sb_qty_lost',
                                                             'Stock Entry Detail-spn_qty_lost',
-                                                            'Stock Entry Detail-spn_cb_qty_lost_1',
-                                                            'Stock Entry Detail-spn_loss_warehouse']]]},
+                                                            'Stock Entry Detail-spn_rejected_qty']]]},
              {"dt": "Custom Script", "filters":[["name", "in", ['Sales Invoice-client','Purchase Receipt-Client','Stock Entry-Client']]]},
              {"dt": "Property Setter", "filters": [["name", "in",["update_stock", "Sales Invoice Item-rate-read_only"]]]},
              {"dt": "Print Format", "filters": [["name", "in", ["SPN Sales Invoice","Memo Invoice","Goods Receipt Note","Stock Transfer Note"]]]}]
