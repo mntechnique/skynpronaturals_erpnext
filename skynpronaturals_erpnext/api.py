0c27f53682f57cd26f84ba22dcb118e34e8df358
Gaurav Naik <gvrvnk@gmail.com>
2017-02-08 12:44:45 +0530

    [New] Basic Tally data import tool for invoices

--- skynpronaturals_erpnext/api.py
+++ skynpronaturals_erpnext/api.py
@@ -1,9 +1,17 @@
+
 from __future__ import unicode_literals
 import frappe, json
 import frappe
 from frappe import _
 from frappe.desk.reportview import get_match_cond
 from erpnext.controllers.queries import get_filters_cond
+from frappe.utils import add_days, nowdate
+from frappe.model.rename_doc import rename_doc
+
+from frappe.utils.pdf import get_pdf
+import pdfkit	
+import os
+
 
 @frappe.whitelist()
 def validate_sales_invoice(self, method):
@@ -300,7 +308,7 @@ def csv_to_json(path, column_headings_row_idx=1, start_parsing_from_idx=2):
 	file_rows = []
 	out_rows = []
 
-	csv_path = path #'/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si.csv' #frappe.utils.get_site_path() + settlement_csv
+	csv_path = frappe.utils.get_site_path() + path #'/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si.csv' #frappe.utils.get_site_path() + settlement_csv
 	#outfile_name = '/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si_out.csv'
 
 	#with open('/home/gaurav/Downloads/25a4cbe4397b494a_2016-12-03_2017-01-02.csv', 'rb') as csvfile:
@@ -322,7 +330,7 @@ def csv_to_json(path, column_headings_row_idx=1, start_parsing_from_idx=2):
 				col = col + "_" + str(count)
 			processed_headings_row.append(col)
 
-		print "PROCESSED HEADINGS", processed_headings_row
+		#print "PROCESSED HEADINGS", processed_headings_row
 
 		for i in xrange(start_parsing_from_idx, len(file_rows)):
 			record_core = ""
@@ -339,7 +347,7 @@ def csv_to_json(path, column_headings_row_idx=1, start_parsing_from_idx=2):
 		#print "FINAL JSON", final_json
 
 #------
-def process_invoices(debit_to="Debtors - SPN", income_ac="Sales - SPN", cost_center="Main - SPN"):
+# def process_invoices(debit_to="Debtors - SPN", income_ac="Sales - SPN", cost_center="Main - SPN"):
 	def process_voucher_no(voucher_no):
 		
 
@@ -389,10 +397,6 @@ def process_invoices(debit_to="Debtors - SPN", income_ac="Sales - SPN", cost_cen
 
 	def stc_template_by_territory(territory):
 		return territory #+ " VAT"
-
-
-
-
 
 	out = []
 	
@@ -511,7 +515,7 @@ def process_invoices(debit_to="Debtors - SPN", income_ac="Sales - SPN", cost_cen
 	print "Total records processed:", processed_recs
 	return out
 	
-def prepsheet_si():
+# def prepsheet_si():
 	import csv
 	
 	rows = process_invoices()
@@ -520,16 +524,16 @@ def prepsheet_si():
 			"~", "charge_type", "account_head", "description", "row_id", "cost_center", "rate", "tax_amount", "total", "tax_amount_after_discount_amount", "base_tax_amount", "base_total", "base_tax_amount_after_discount_amount"]
 
 	with open('/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si_out.csv', 'w') as csvfile:
-	    fieldnames = column_headings_row #['last_name', 'first_name']
-	    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		fieldnames = column_headings_row #['last_name', 'first_name']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-	    writer.writeheader()
-	    for row in rows:
-	    	writer.writerow(row)
+		writer.writeheader()
+		for row in rows:
+			writer.writerow(row)
 
 
 #Stock entry
-def process_stock_entries():
+# def process_stock_entries():
 
 	def get_processed_txn_no_and_series(txn_no):
 		return {
@@ -651,36 +655,307 @@ def process_stock_entries():
 	#print processed_recs	
 	return out
 
-def prepsheet_stock_entry():
+# def prepsheet_stock_entry():
 	import csv
 
 	rows = process_stock_entries()
 	column_headings_row = ["name", "naming_series", "purpose", "company", "posting_date", "posting_time", "~", "item_code", "description", "qty", "uom", "conversion_factor", "stock_uom", "transfer_qty", "s_warehouse", "t_warehouse", "~"]
 
 	with open('/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_stockentry_out.csv', 'w') as csvfile:
-	    fieldnames = column_headings_row
-	    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		fieldnames = column_headings_row
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-	    writer.writeheader()
-	    for row in rows:
-	    	writer.writerow(row)
+		writer.writeheader()
+		for row in rows:
+			writer.writerow(row)
+
+@frappe.whitelist()
+def process_sheet_and_create_si(path_to_sheet, debit_to="Debtors - SPN", income_ac="Sales - SPN", cost_center="Main - SPN"):
+	from frappe.model.rename_doc import rename_doc
+
+	def process_voucher_no(voucher_no):
+		naming_series = voucher_no[:-4] + "-.#####"
+		processed_voucher_no = voucher_no[:-4] + "-" + voucher_no[-4:].zfill(5) 
+		warehouse = ""
+
+		#print "naming series", voucher_no[:-4].lower()
+			
+		if voucher_no[:-4].lower() in ["bc","bv","bu"]:
+			warehouse = "(MAHARASHTRA, Bhiwandi) Bellezimo Professionale Products Pvt. Ltd. C/o. Kotecha Clearing & Forwarding Pvt. Ltd.  - SPN"
+		elif voucher_no[:-4].lower() in ["gv","gc", "gu"]:
+			warehouse = "(ASSAM, Guwahati) Bellezimo Professionale Products Pvt. Ltd. C/o. Siddhi Vinayak Agencies - SPN"
+		elif voucher_no[:-4].lower() in ["wbc","wbv","wbu"]:
+			warehouse = "(WEST BENGAL, Kolkata) Bellezimo Professionale Products Pvt. Ltd. C/o. Alloy Associates - SPN"
+		else:
+			warehouse = ""
+
+		#print "Voucher No: ", processed_voucher_no, "Naming Series: ", naming_series, "Warehouse: ", warehouse
+
+		return processed_voucher_no, naming_series, warehouse
+
+	def percentage_by_voucher_no(voucher_no):
+		if "bc" in voucher_no.lower():
+			return 2.0
+		elif "bv" in voucher_no.lower():
+			return 13.5
+		elif "gv" in voucher_no.lower():
+			return 15.0
+		elif "gc" in voucher_no.lower():
+			return 2.0
+		elif "wbv" in voucher_no.lower():
+			return 15.0
+		elif "wbc" in voucher_no.lower():
+			return 2.0
+		else:
+			return None
+
+	# def price_list_by_territory(territory):
+	# 	# if territory == "West Bengal":
+	# 	# 	return "West Bengal"
+	# 	# elif territory == "Gujarat":
+	# 	# 	return "Gujarat Registered Distributor"
+	# 	# else:
+	# 	return territory
+
+	# def account_head_by_territory(territory):
+	# 	return territory #+ " VAT - SPN"
+
+	# def stc_template_by_territory(territory):
+	# 	return territory #+ " VAT"
+
+	def stc_template_by_naming_series_tax_percentage(voucher_no, tax_pct):
+		if "bc" in voucher_no.lower():
+			return "Maharashtra CST {0}%".format(tax_pct).replace(".0","")
+		elif ("wbv" in voucher_no.lower()) or ("wbu" in voucher_no.lower()):
+			return "West Bengal VAT {0}%".format(tax_pct).replace(".0","")
+		elif "wbc" in voucher_no.lower():
+			return "West Bengal CST {0}%".format(tax_pct).replace(".0","")
+		elif ("bv" in voucher_no.lower()) or ("bu" in voucher_no.lower()):
+			return "Maharashtra VAT {0}%".format(tax_pct).replace(".0","")
+		elif ("gv" in voucher_no.lower()) or ("gu" in voucher_no.lower()):
+			return "Assam VAT {0}%".format(tax_pct).replace(".0","")
+		elif "gc" in voucher_no.lower():
+			return "Assam CST {0}%".format(tax_pct).replace(".0","")
+		else:
+			return None
+	
+	def account_head_by_naming_series(voucher_no):
+		if "bc" in voucher_no.lower():
+			return "Maharashtra CST - SPN"
+		elif ("wbv" in voucher_no.lower()) or ("wbu" in voucher_no.lower()):
+			return "West Bengal VAT - SPN"
+		elif "wbc" in voucher_no.lower():
+			return "West Bengal CST - SPN"
+		elif ("bv" in voucher_no.lower()) or ("bu" in voucher_no.lower()):
+			return "Maharashtra VAT - SPN"
+		elif ("gv" in voucher_no.lower()) or ("gu" in voucher_no.lower()):
+			return "Assam VAT - SPN"
+		elif "gc" in voucher_no.lower():
+			return "Assam CST - SPN"
+		else:
+			return None	
+
+	def price_list_by_customer_or_net_total(customer, net_total):
+
+		cg = frappe.db.get_value("Customer", {"name": customer}, "customer_group")
+
+		if net_total <= 5.0: ##Less than two means memo invoice.
+			return "MEMO/ZERO INVOICING"
+
+		print "Customer Group", cg
+
+		if ("maharashtra" in cg.lower()) or ("maharastra" in cg.lower()):
+			return "Maharashtra"
+		elif "west bengal" in cg.lower():
+			return "West Bengal (VAT)"
+		elif "assam" in cg.lower():
+			return "Assam"
+		elif "gujarat" in cg.lower():
+			if "registered" in cg.lower():
+				return "Gujarat Registered Distributor"
+			elif "unregistered" in cg.lower():
+				return "Gujarat Unregistered Distributor"
+		else:
+			return cg
+
+	def get_corrected_territory(territory):
+		if "maha" in territory.lower():
+			return "Maharashtra"
+		elif "delhi" in territory.lower():
+			return "Delhi"
+		else:
+			return territory
+
+	msgs = []
+
+	out = []
+	
+	final_json = csv_to_json(path=path_to_sheet) #'/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si.csv'
+	rows = final_json["data"]
+
+	unique_vouchers = list(set([v.get("Voucher No") for v in rows]))
+	#unique_vouchers = []
+
+	processed_recs = 0
+	for uv in unique_vouchers:
+		rowmsg = []
+		net_total = sum([float(i.get("Quantity")) * float(i.get("Rate")) for i in rows if i.get("Voucher No") == uv])
+		grand_total = sum([
+					(float(i.get("Quantity")) * float(i.get("Rate"))) + 
+					((float(i.get("Quantity")) * float(i.get("Rate"))) * (percentage_by_voucher_no(i.get("Voucher No")) if i.get("Percentage") == "null" else float(i.get("Percentage")) / 100)) for i in rows if i.get("Voucher No") == uv])
 
 
-# def read_csv(path, column_headings_row_idx=0):
-# 	file_rows = []
-# 	out_rows = []
+		if net_total < 0:
+			continue
 
-# 	csv_path = path #'/home/gaurav/gaurav-work/skynpro/Tally2ERPNext/skynpro_tally_si.csv' #frappe.utils.get_site_path() + settlement_csv
+		voucher_no, naming_series, warehouse = process_voucher_no(uv)
 
-# 	with open(csv_path, 'rb') as csvfile:
-# 		rdr = csv.reader(csvfile, delimiter=str(','), quotechar=str('"'))
-	   
-# 		for row in rdr:
-# 			file_rows.append(row)
-
-# 		column_headings_row = file_rows[column_headings_row_idx]
-
-# 		for i in xrange(start_parsing_from_idx, len(file_rows)):
+		rowmsg.append("Voucher No: {0}".format(uv))
+		print "Voucher No: ", uv, "Processed voucher no: ", voucher_no 
 
 
+		if frappe.db.get_value("Sales Invoice", {"name": voucher_no}, "name"):
+			rowmsg.append("{0} already exists".format(voucher_no))
+			print voucher_no, " already exists."
+			continue
 
+		#newrow.update({"name": voucher_no})
+		#newrow.update({"naming_series": naming_series})
+
+		voucher_items = [i for i in rows if i.get("Voucher No") == uv]
+
+		if not frappe.db.get_value("Customer", {"name": voucher_items[0]["Party Name"]}, "name"):
+			rowmsg.append("Customer '{0}' not found".format(voucher_items[0]["Party Name"]))
+			print "Customer ", voucher_items[0]["Party Name"], " not found."
+			continue
+
+		percentage = (float(voucher_items[0]["Percentage"] if voucher_items[0]["Percentage"] != "null" else 0.0))
+		#warehouse = frappe.db.sql("""select name from tabWarehouse where LOWER(state) = "{0}" """.format(voucher_items[0]["State"].lower()), as_dict=True)
+
+		#print "WAREHOUSE", warehouse
+
+		si = frappe.new_doc("Sales Invoice")
+		si.naming_series = naming_series
+		si.posting_date = frappe.utils.getdate(voucher_items[0]["Date"])
+		si.company = "Bellezimo Professionale Products Pvt. Ltd."
+		si.customer = voucher_items[0]["Party Name"]
+		si.currency = "INR"
+		si.conversion_rate = 1.0
+		si.selling_price_list = price_list_by_customer_or_net_total(voucher_items[0]["Party Name"], net_total)
+
+		# rowmsg.append("Selling Price List: {0}".format(si.selling_price_list))
+		# print "Selling Price List", si.selling_price_list
+
+		si.price_list_currency = "INR"
+		si.plc_conversion_rate = 1.0
+		si.base_net_total = net_total
+		si.base_grand_total = grand_total
+		si.grand_total = grand_total
+		si.debit_to = debit_to
+		si.c_form_applicable = "Yes" if (voucher_items[0].get("Form Name") == "C") else "No"
+		si.is_return = 1 if (grand_total < 0) else 0
+		si.due_date = frappe.utils.getdate(voucher_items[0]["Date"])
+		si.territory = get_corrected_territory(voucher_items[0]["State_1"])
+		si.taxes_and_charges = stc_template_by_naming_series_tax_percentage(voucher_no, percentage)
+		si.spn_warehouse = warehouse #frappe.db.get_value("Warehouse", {"state": voucher_items[0]["State"]})
+	
+
+		for item in voucher_items:
+
+			si.append("items", {
+				"item_code": item["Stock Item Alias"],
+				"item_name": item["Item Name"],
+				"description": item["Item Name"],
+				"qty": float(item["Quantity"]),
+				"rate": float(item["Rate"]),
+				"amount": float(item["Quantity"]) * float(item["Rate"]),
+				"base_rate": float(item["Rate"]),
+				"base_amount": float(item["Quantity"]) * float(item["Rate"]),
+				"income_account": income_ac,
+				"cost_center": cost_center,
+				"price_list_rate": frappe.db.get_value("Item Price", {"price_list": si.selling_price_list, "item_name": item["Stock Item Alias"] }, "price_list_rate"),
+				"warehouse": warehouse,
+			})
+
+		si.append("taxes", {
+			"charge_type": "On Net Total",
+			"account_head": account_head_by_naming_series(voucher_no),
+			"description": account_head_by_naming_series(voucher_no),
+			"cost_center": cost_center,
+			"rate": percentage,
+			"tax_amount": net_total * (percentage/100),
+			"total": grand_total,
+			"tax_amount_after_discount_amount": grand_total,
+			"base_tax_amount": net_total * percentage,
+			"base_total": grand_total,
+			"base_tax_amount_after_discount_amount": net_total * (percentage/100)	
+		})
+
+		try:
+			si.save()
+			frappe.db.commit()
+			print "Saved as", si.name
+			rowmsg.append("Invoice Saved. ({0})".format(si.name))
+			rename_doc("Sales Invoice", si.name, voucher_no, force=True)
+			print "Renamed to", voucher_no
+			rowmsg.append("Renamed to {0}".format(voucher_no))
+		except Exception as e:
+			print "SI '" + voucher_no + "' not saved. {0}".format(e)
+			rowmsg.append("Invoice '{0}' not saved: {1}".format(voucher_no, e))
+
+		processed_recs += 1
+
+		#rowmsg.append("Records processed {0}".format(processed_recs))
+		msgs.append("\n".join(rowmsg))
+
+		if processed_recs == 5:
+			break
+
+	print "Total records processed:", processed_recs
+	msgs.append("Total records processed: {0}".format(processed_recs))
+
+	content = "\n".join(msgs)
+
+	# frappe.local.response.filename = "{filename}.pdf".format(filename="si_log_{0}".format(frappe.utils.get_datetime()))
+	# frappe.local.response.filecontent = get_log_dump(content)
+	# frappe.local.response.type = "download"
+
+	# #Print rowmsg
+	# with open("/home/frappe/si_log.txt", "w") as txtfile:
+	# 	
+	# 	txtfile.write(content)
+
+	return content
+
+
+
+def get_log_dump(filecontent):
+	fname = os.path.join("/tmp", "si-import-log-{0}.pdf".format(frappe.generate_hash()))
+
+	try:
+		pdfkit.from_string(filecontent, fname, options=options or {})
+
+		with open(fname, "rb") as fileobj:
+			filedata = fileobj.read()
+
+	except IOError, e:
+		if ("ContentNotFoundError" in e.message
+			or "ContentOperationNotPermittedError" in e.message
+			or "UnknownContentError" in e.message
+			or "RemoteHostClosedError" in e.message):
+
+			# allow pdfs with missing images if file got created
+			if os.path.exists(fname):
+				with open(fname, "rb") as fileobj:
+					filedata = fileobj.read()
+
+			else:
+				frappe.throw(_("PDF generation failed because of broken image links"))
+		else:
+			raise
+
+	finally:
+		cleanup(fname)
+
+
+	return filedata
---
+++ skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/spn_tally_data_import_tool/spn_tally_data_import_tool.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2016, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SPN Tally Data Import Tool', {
+	refresh: function(frm) {
+
+	},
+	btn_import: function(frm) {
+		frappe.call({
+			method: "skynpronaturals_erpnext.api.process_sheet_and_create_si",
+			args: { 
+				path_to_sheet: frm.doc.attach_sheet
+			},
+			freeze: true,
+			freeze_message: "Importing...",
+			callback: function(r) {
+				if(r) {
+					console.log(r.message);
+					cur_frm.set_value("import_log", r.message);
+				}
+			}
+		})
+	}
+});
