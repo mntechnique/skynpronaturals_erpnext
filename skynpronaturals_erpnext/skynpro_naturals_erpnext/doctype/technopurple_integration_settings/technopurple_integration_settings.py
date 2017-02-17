73e47a018150b21c16d7de4b1e7f2c67b46658f7
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-02-17 17:12:31 +0530

    TechnoPurple Integration Settings Doctype
    Added Fields
    name: sb_tp_api_key,  type: Section Break, label: "TechnoPurple API Key"
    name: tp_api_key, type: Data, ReadOnly:true

---
+++ skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/technopurple_integration_settings/technopurple_integration_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, MN Technique and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class TechnoPurpleIntegrationSettings(Document):
+	def on_update(self):
+		frappe.add_version(self)
