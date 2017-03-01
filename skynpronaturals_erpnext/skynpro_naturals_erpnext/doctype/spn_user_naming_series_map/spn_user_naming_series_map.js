8a18ce1243fd7e0318ae590b00ace20db044ee18
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-03-01 17:19:17 +0530

    Added user restrictions for Naming Series:
    [Fix] Two child doctypes:Sales Naming Series Map, Purchase Naming Series Map
    [New] Naming Series select field shows option according to mapped doctype

--- skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/spn_user_naming_series_map/spn_user_naming_series_map.js
+++ skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/spn_user_naming_series_map/spn_user_naming_series_map.js
@@ -3,32 +3,40 @@
 
 frappe.ui.form.on('SPN User Naming Series Map', {
 	onload: function(frm) {
-        set_query_doctype(frm);
+        // set_query_doctype(frm);
     },
 	refresh: function(frm) {
-		// frm.set_df_property("naming_series", "options", "a\nb\nc", null, "naming_series_map")	
 		frappe.call({
 			method: "skynpronaturals_erpnext.api.get_list",
 			args:{"dt":"Sales Invoice"},
 			callback:function(r){
-				cur_frm.fields_dict.naming_series_map.grid.get_docfield("naming_series").options = r.message.prefixes;
-				refresh_fields();
+				cur_frm.fields_dict.sales_naming_series_map.grid.get_docfield("naming_series").options = r.message.prefixes;
+				refresh_field();
+				// set_query_doctype(cur_frm,"Sales Invoice");
+			}
+		});
+		frappe.call({
+			method: "skynpronaturals_erpnext.api.get_list",
+			args:{"dt":"Purchase Invoice"},
+			callback:function(r){
+				cur_frm.fields_dict.purchase_naming_series_map.grid.get_docfield("naming_series").options = r.message.prefixes;
+				refresh_field();
+				// set_query_doctype(cur_frm,"Purchase Invoice");
 			}
 		});		
-		set_query_doctype(frm);
     }
 });
 
-function set_query_doctype(frm){
-    frm.set_query("map_doctype", "naming_series_map", function() {
-        return {
-        	query: "",
-            filters: [
-                ["DocType", "name", "in", ["Sales Invoice","Purchase Receipt"]]
-            ]
-        }
-    });
-}
+// function set_query_doctype(frm,map_docname){
+//     frm.set_query("map_doctype", "naming_series_map", function() {
+//         return {
+//         	query: "",
+//             filters: [
+//                 ["DocType", "name", "in", [map_docname]]
+//             ]
+//         }
+//     });
+// }
 
 // list = ["D.RM-PREC-.#####","D.RM-PREC-RET-.#####","SINV-D.RM-.#####"];
 // cur_frm.set_df_property("naming_series", "options", list.join("\n"), null, "naming_series_map");
