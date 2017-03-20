669742968f8225dcc055aab52ccf98ff8bc5d40e
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-03-20 15:38:10 +0530

    [Fix] Renamed Material Receipt&Issue doctypes, deleted uncessary doctypes

--- skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/spn_user_stock_entry_material_receipt_naming_series_map_item/spn_user_stock_entry_material_receipt_naming_series_map_item.py
+++ skynpronaturals_erpnext/skynpro_naturals_erpnext/doctype/spn_user_stock_entry_material_receipt_naming_series_map_item/spn_user_stock_entry_material_receipt_naming_series_map_item.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class SPNUserStockEntry-MaterialReceiptNamingSeriesMapItem(Document):
+class SPNUserStockEntryMaterialReceiptNamingSeriesMapItem(Document):
 	pass
